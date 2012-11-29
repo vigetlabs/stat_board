9065e3aaa5e25907c24aaea528e0058193bec5c4
David Eisinger <david.eisinger@gmail.com>
2012-11-29 17:07:01 -0500

    Add Bootstrap to asset pipeline

--- lib/stat_board/engine.rb
+++ lib/stat_board/engine.rb
@@ -1,5 +1,11 @@
 module StatBoard
   class Engine < ::Rails::Engine
     isolate_namespace StatBoard
+
+    if Rails.version > "3.1"
+      initializer "StatBoard precompile hook" do |app|
+        app.config.assets.precompile += %w(stat_board/bootstrap.css)
+      end
+    end
   end
 end
