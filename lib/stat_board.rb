5443a76c2c7314991ce86cdf04ad5348cd4d79c9
Accessd <accessd0@gmail.com>
2014-07-09 11:31:55 +0400

    cache statistics

--- lib/stat_board.rb
+++ lib/stat_board.rb
@@ -6,8 +6,9 @@ require "stat_board/reports/weekly_report"
 require "stat_board/graph_helper"
 
 module StatBoard
-  mattr_accessor :models, :title, :display_graph, :username, :password
+  mattr_accessor :models, :title, :display_graph, :username, :password, :cache_lifetime
 
   # Display graph by default. Can be disabled in initializers/stat_board.rb.
   @@display_graph = true
+  @@cache_lifetime = 1.hour
 end
